80ca38083aba36f2bf4914776d3c22df2fdf3b48
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-06-16 07:41:33 -0300

    Adição do modal

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-import initTabNav from './modules/tab-nav.js'
-import initAccordion from './modules/accordion.js'
-import initScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/animacao-scroll.js'
+import initTabNav from './modules/tab-nav.js';
+import initAccordion from './modules/accordion.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initModal from './modules/modal.js';
+
 
 
 initTabNav(); 
 initAccordion(); 
 initScrollSuave(); 
 initAnimacaoScroll();
+initModal();
