5db58f242de57639d09971dcb457f51461e407dc
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-07-06 04:11:49 -0300

    Oraganizando projeto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-import initTabNav from './modules/tab-nav.js';
-import initAccordion from './modules/accordion.js';
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -9,13 +8,15 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 initTabNav();
 
 initAccordion();
 
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
 
 initAnimacaoScroll();
 
