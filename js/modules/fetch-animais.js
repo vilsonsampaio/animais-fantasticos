5bf5d46155d1b3239b2e12741b45de43115be00a
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-07-06 06:23:01 -0300

    AnimaNumero refatorado (transformado em classe)

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,11 +1,19 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+
+    return div;
+  }
+
   async function fetchAnimais(url) {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector(".numeros-grid");
+      const numerosGrid = document.querySelector('.numeros-grid');
 
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
@@ -13,19 +21,14 @@ export default function initFetchAnimais() {
       });
 
       // Iniciando o efeito de animar números apenas quando o fetch ocorrer;
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
   }
 
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
 
-    return div;
-  }
 
   fetchAnimais("../../animaisapi.json");
 }
