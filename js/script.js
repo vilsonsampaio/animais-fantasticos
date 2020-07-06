045640f1820cfcb0d463ac312951e9c40dec5dc5
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-07-06 04:05:18 -0300

    ScrollSuave refatorado (transformado em classe)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initTabNav from './modules/tab-nav.js';
 import initAccordion from './modules/accordion.js';
-import initScrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -10,18 +10,25 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
+initTabNav();
 
+initAccordion();
 
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
-
-
-initTabNav(); 
-initAccordion(); 
-initScrollSuave(); 
 initAnimacaoScroll();
+
 initModal();
+
 initTooltip();
+
 initDropdownMenu();
+
 initMenuMobile();
-initFuncionamento();initFetchAnimais();
+
+initFuncionamento();
+
+initFetchAnimais();
+
 initFetchBitcoin();
